f020e35cb862dd6a0c8bac45a302d4edb981ba21
KaidLi <zhujianghaoshi@gmail.com>
2024-09-27 21:43:02 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -19,9 +19,6 @@ module.exports = {
   // social usernames (optional)
   twitter: null,
   github: null,
-  wechatPublicName: null
-  wechatPublicURL: null,
-  notionPublic: null,
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
